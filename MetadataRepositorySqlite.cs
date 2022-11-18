7854042cfbfcf5f2f7e0355f1fbb5b6243b92314
rjwehrle <rjwehrle@gmail.com>
2022-11-17 21:13:29 -0500

    Initial sqlite repository

---
+++ MetadataRepositorySqlite.cs
@@ -0,0 +1,165 @@
+﻿using Famecipe.Models;
+using Famecipe.Common;
+using Microsoft.Data.Sqlite;
+
+namespace Famecipe.Repository.Sqlite;
+
+public class MetadataRepositorySqlite : IRepository<Metadata>
+{
+    private readonly string _metadataConnectionString;
+
+    public MetadataRepositorySqlite()
+    {
+        var metadataDatabase = Environment.GetEnvironmentVariable("METADATA_DATA_SOURCE");
+
+        if (string.IsNullOrEmpty(metadataDatabase))
+        {
+            this._metadataConnectionString = $"Data Source=Metadata.db";
+        }
+        else
+        {
+            this._metadataConnectionString = $"Data Source={metadataDatabase}";
+        }
+
+        using (var connection = new SqliteConnection(this._metadataConnectionString))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE IF NOT EXISTS Metadata (Key TEXT, Value TEXT);
+            ";
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public async Task<Metadata> Create(Metadata metadata)
+    {
+        using (var connection = new SqliteConnection(this._metadataConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                INSERT INTO Metadata
+                VALUES($Key, $Value)
+            ";
+            command.Parameters.AddWithValue("$Key", metadata.Key);
+            command.Parameters.AddWithValue("$Value", System.Text.Json.JsonSerializer.Serialize<object>(metadata.Value!));
+
+            await command.ExecuteNonQueryAsync();
+
+            return await Get(metadata.Key!);
+        }
+    }
+
+    public async Task Delete(string key)
+    {
+        using (var connection = new SqliteConnection(this._metadataConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                DELETE
+                FROM Metadata
+                WHERE Key = $Key
+            ";
+            command.Parameters.AddWithValue("$Key", key);
+
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+
+    public async Task<IEnumerable<Metadata>> Get()
+    {
+        using (var connection = new SqliteConnection(this._metadataConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT *
+                FROM Metadata
+            ";
+
+            var metadata = new List<Metadata>();
+
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                var keyOrdinal = reader.GetOrdinal("Key");
+                var valueOrdinal = reader.GetOrdinal("Value");
+
+                while (await reader.ReadAsync())
+                {
+                    metadata.Add(new Metadata()
+                    {
+                        Key = reader.GetString(keyOrdinal),
+                        Value = System.Text.Json.JsonSerializer.Deserialize<object>(reader.GetString(valueOrdinal))
+                    });
+                }
+            }
+
+            return metadata;
+        }
+    }
+
+    public async Task<Metadata> Get(string key)
+    {
+        using (var connection = new SqliteConnection(this._metadataConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT *
+                FROM Metadata
+                WHERE Key = $Key
+            ";
+            command.Parameters.AddWithValue("$Key", key);
+
+            Metadata metadata = null;
+
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                var keyOrdinal = reader.GetOrdinal("Key");
+                var valueOrdinal = reader.GetOrdinal("Value");
+
+                while (await reader.ReadAsync())
+                {
+                    metadata = new Metadata()
+                    {
+                        Key = reader.GetString(keyOrdinal),
+                        Value = System.Text.Json.JsonSerializer.Deserialize<object>(reader.GetString(valueOrdinal))
+                    };
+                }
+            }
+
+            return metadata;
+        }
+    }
+
+    public async Task Update(string key, Metadata metadata)
+    {
+        using (var connection = new SqliteConnection(this._metadataConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                UPDATE Metadata
+                SET Value = $Value
+                WHERE Key = $Key
+            ";
+            command.Parameters.AddWithValue("$Key", key);
+            command.Parameters.AddWithValue("$Value", System.Text.Json.JsonSerializer.Serialize<object>(metadata.Value!));
+
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
---
+++ RecipeRepositorySqlite.cs
@@ -0,0 +1,201 @@
+﻿using Famecipe.Models;
+using Famecipe.Common;
+using Microsoft.Data.Sqlite;
+using System.Globalization;
+
+namespace Famecipe.Repository.Sqlite;
+
+public class RecipeRepositorySqlite : IRepository<Recipe>
+{
+    private readonly string _recipesConnectionString;
+
+    public RecipeRepositorySqlite()
+    {
+        var recipeDatabase = Environment.GetEnvironmentVariable("RECIPES_DATA_SOURCE");
+
+        if (string.IsNullOrEmpty(recipeDatabase))
+        {
+            this._recipesConnectionString = $"Data Source=Recipe.db";
+        }
+        else
+        {
+            this._recipesConnectionString = $"Data Source={recipeDatabase}";
+        }
+
+        using (var connection = new SqliteConnection(this._recipesConnectionString))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE IF NOT EXISTS Recipe (Identifier TEXT, Name TEXT, Image TEXT, Ingredients TEXT, Equipment TEXT, Directions TEXT, WhenUpdatedUTC TEXT);
+            ";
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public async Task<Recipe> Create(Recipe recipe)
+    {
+        using (var connection = new SqliteConnection(this._recipesConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                INSERT INTO Recipe
+                VALUES($Identifier, $Name, $Image, $Ingredients, $Equipment, $Directions, $WhenUpdatedUTC)
+            ";
+            command.Parameters.AddWithValue("$Identifier", recipe.Identifier);
+            command.Parameters.AddWithValue("$Name", recipe.Name);
+            command.Parameters.AddWithValue("$Image", recipe.Image);
+            command.Parameters.AddWithValue("$Ingredients", string.Join('|', recipe.Ingredients ?? new List<string>()));
+            command.Parameters.AddWithValue("$Equipment", string.Join('|', recipe.Equipment ?? new List<string>()));
+            command.Parameters.AddWithValue("$Directions", string.Join('|', recipe.Directions ?? new List<string>()));
+            command.Parameters.AddWithValue("$WhenUpdatedUTC", recipe.WhenUpdatedUTC!.Value.ToString("o", CultureInfo.InvariantCulture));
+
+            await command.ExecuteNonQueryAsync();
+
+            return await Get(recipe.Identifier!);
+        }
+    }
+
+    public async Task Delete(string identifier)
+    {
+        using (var connection = new SqliteConnection(this._recipesConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                DELETE
+                FROM Recipe
+                WHERE Identifier = $Identifier
+            ";
+            command.Parameters.AddWithValue("$Identifier", identifier);
+
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+
+    public async Task<IEnumerable<Recipe>> Get()
+    {
+        using (var connection = new SqliteConnection(this._recipesConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT *
+                FROM Recipe
+            ";
+
+            var recipes = new List<Recipe>();
+
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                var identifierOrdinal = reader.GetOrdinal("Identifier");
+                var nameOrdinal = reader.GetOrdinal("Name");
+                var imageOrdinal = reader.GetOrdinal("Image");
+                var ingredientsOrdinal = reader.GetOrdinal("Ingredients");
+                var equipmentOrdinal = reader.GetOrdinal("Equipment");
+                var directionsOrdinal = reader.GetOrdinal("Directions");
+                var whenUpdatedUTCOrdinal = reader.GetOrdinal("WhenUpdatedUTC");
+
+                while (await reader.ReadAsync())
+                {
+                    recipes.Add(new Recipe()
+                    {
+                        Identifier = reader.GetString(identifierOrdinal),
+                        Name = reader.GetString(nameOrdinal),
+                        Image = reader.GetString(imageOrdinal),
+                        Ingredients = reader.GetString(ingredientsOrdinal).Split('|').ToList(),
+                        Equipment = reader.GetString(equipmentOrdinal).Split('|').ToList(),
+                        Directions = reader.GetString(directionsOrdinal).Split('|').ToList(),
+                        WhenUpdatedUTC = reader.GetDateTime(whenUpdatedUTCOrdinal)
+                    });
+                }
+            }
+
+            return recipes;
+        }
+    }
+
+    public async Task<Recipe> Get(string identifier)
+    {
+        using (var connection = new SqliteConnection(this._recipesConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT *
+                FROM Recipe
+                WHERE Identifier = $Identifier
+            ";
+            command.Parameters.AddWithValue("$Identifier", identifier);
+
+            Recipe recipe = null;
+
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                var identifierOrdinal = reader.GetOrdinal("Identifier");
+                var nameOrdinal = reader.GetOrdinal("Name");
+                var imageOrdinal = reader.GetOrdinal("Image");
+                var ingredientsOrdinal = reader.GetOrdinal("Ingredients");
+                var equipmentOrdinal = reader.GetOrdinal("Equipment");
+                var directionsOrdinal = reader.GetOrdinal("Directions");
+                var whenUpdatedUTCOrdinal = reader.GetOrdinal("WhenUpdatedUTC");
+
+                while (await reader.ReadAsync())
+                {
+                    recipe = new Recipe()
+                    {
+                        Identifier = reader.GetString(identifierOrdinal),
+                        Name = reader.GetString(nameOrdinal),
+                        Image = reader.GetString(imageOrdinal),
+                        Ingredients = reader.GetString(ingredientsOrdinal).Split('|').ToList(),
+                        Equipment = reader.GetString(equipmentOrdinal).Split('|').ToList(),
+                        Directions = reader.GetString(directionsOrdinal).Split('|').ToList(),
+                        WhenUpdatedUTC = reader.GetDateTime(whenUpdatedUTCOrdinal)
+                    };
+                }
+            }
+
+            return recipe;
+        }
+    }
+
+    public async Task Update(string identifier, Recipe recipe)
+    {
+        using (var connection = new SqliteConnection(this._recipesConnectionString))
+        {
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                UPDATE Recipe
+                SET Name = $Name,
+                    Image = $Image,
+                    Ingredients = $Ingredients,
+                    Equipment = $Equipment,
+                    Directions = $Directions,
+                    WhenUpdatedUTC = $WhenUpdatedUTC
+                WHERE Identifier = $Identifier
+            ";
+            command.Parameters.AddWithValue("$Identifier", identifier);
+            command.Parameters.AddWithValue("$Name", recipe.Name);
+            command.Parameters.AddWithValue("$Image", recipe.Image);
+            command.Parameters.AddWithValue("$Ingredients", string.Join('|', recipe.Ingredients ?? new List<string>()));
+            command.Parameters.AddWithValue("$Equipment", string.Join('|', recipe.Equipment ?? new List<string>()));
+            command.Parameters.AddWithValue("$Directions", string.Join('|', recipe.Directions ?? new List<string>()));
+            command.Parameters.AddWithValue("$WhenUpdatedUTC", recipe.WhenUpdatedUTC!.Value.ToString("o", CultureInfo.InvariantCulture));
+
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
